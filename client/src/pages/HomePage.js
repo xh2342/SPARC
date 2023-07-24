b1f8b4ead2dbf0b78bf8a8ec28c550ad71679fb2
Xiaofei Hwang <110270119+Xiaofei-Huang@users.noreply.github.com>
2023-07-24 09:50:39 -0400

    constructed client folder

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { Container, Divider, Link } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const config = require('../config.json');
+
+export default function HomePage() {
+  return (
+    <Container>
+      <h2>Home Page</h2>
+    </Container>
+  );
+};
